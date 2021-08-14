be655245ab0addb85a2b9e37e133a5732b99249f
Roland Levy <contact@rolandlevy.co.uk>
2021-08-14 19:57:18 +0100

    Added conditional wrapper

--- src/SimpleBackdrop.js
+++ src/SimpleBackdrop.js
@@ -4,6 +4,12 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
+const ConditionalWrapper = ({ condition, wrapper, children }) => {
+  return (
+    {}
+  );
+};
+
 const useStyles = makeStyles((theme) => ({
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
